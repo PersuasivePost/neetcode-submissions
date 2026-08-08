ed83fa65d671c1e343f2c9ff5ad3bbe64196d7c4
ASHVATTH JOSHI <codingash27@gmail.com>
2026-08-08 22:21:59 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // find intersection point
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // find entrance 
+        int slow2 = nums[0];
+        while (slow2 != slow) {
+            slow2 = nums[slow2];
+            slow = nums[slow];
+        }
+
+        return slow2;
+    }
+};
